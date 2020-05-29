254e328a7d7015159351d0948b00eec1cf3a0e42
Yao Josué <jyao2000@yahoo.fr>
2020-05-29 09:27:07 +0200

    previous and next image

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  int photoIndex = 0;
+  List<String> photos = [
+    'assets/burger1.jpg',
+    'assets/burger2.jpg',
+    'assets/burger3.jpg',
+    'assets/burger4.jpg',
+  ];
+
+  void _previousImage(){
+    setState(() {
+      photoIndex = photoIndex > 0 ? photoIndex -1 : 0;
+    });
+  }
+
+  void _nextImage(){
+    setState(() {
+      photoIndex = photoIndex < photos.length - 1 ? photoIndex + 1 : photoIndex;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
